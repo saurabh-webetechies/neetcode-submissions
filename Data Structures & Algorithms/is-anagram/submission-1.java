f3dd0020ea556ccd370459ab48e12fd45806aad9
saurabh-webetechies <saurabh.singh@webetechies.com>
2026-06-01 07:02:46 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+      Map<Character,Integer> map = new HashMap<>();
+
+      for(int i = 0;i<s.length(); i++){
+        map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+      }
+
+      for(int i = 0; i<t.length(); i++){
+        if(map.containsKey(t.charAt(i))){
+           map.put(t.charAt(i),map.get(t.charAt(i))-1); 
+        }
+      }
+      for(Map.Entry<Character,Integer> entry:map.entrySet()){
+        if(entry.getValue() < 0 || entry.getValue() > 0){
+            return false;
+        }
+      }
+      return true;
+    }
+}
